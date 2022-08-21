171503a434c44bd543990d2848aaa331ab379f0c
Aleks <heoh888@ya.ru>
2022-08-21 19:08:25 +0300

    Initial Commit

---
+++ Сreation wrapping/_reation_wrappingApp.swift
@@ -0,0 +1,17 @@
+//
+//  _reation_wrappingApp.swift
+//  Сreation wrapping
+//
+//  Created by Алексей Ходаков on 21.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct _reation_wrappingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
